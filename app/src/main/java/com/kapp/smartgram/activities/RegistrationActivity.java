3ca24df0b12408a6a39778000440eaf81849de8f
jayaprasad356 <jayaprasad356@gmail.com>
2022-10-28 19:53:35 +0530

    minor changes done

--- app/src/main/java/com/kapp/smartgram/activities/RegistrationActivity.java
+++ app/src/main/java/com/kapp/smartgram/activities/RegistrationActivity.java
@@ -1,5 +1,6 @@
 package com.kapp.smartgram.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -7,15 +8,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.android.material.slider.Slider;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.gson.Gson;
+import com.kapp.smartgram.MainActivity;
 import com.kapp.smartgram.OtpActivity;
 import com.kapp.smartgram.R;
 import com.kapp.smartgram.helper.ApiConfig;
@@ -28,6 +45,9 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import in.aabhasjindal.otptextview.OtpTextView;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -37,6 +57,11 @@ public class RegistrationActivity extends AppCompatActivity {
     EditText etName,etMobile,etPassword,etConfirmPassword,etEmail,etAddress,etPincode,etOccupation;
     Button btnRegister;
     Activity activity;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+    private static final String TAG = "OTP";
+    private FirebaseAuth mAuth;
+    private String mVerificationId = "";
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,6 +82,7 @@ public class RegistrationActivity extends AppCompatActivity {
         etPincode = findViewById(R.id.etPincode);
         spinDistrict = findViewById(R.id.spinDistrict);
         btnRegister = findViewById(R.id.btnRegister);
+        mAuth = FirebaseAuth.getInstance();
 
         backimg.setOnClickListener(v -> onBackPressed());
         defaultspinnerList();
@@ -141,7 +167,8 @@ public class RegistrationActivity extends AppCompatActivity {
                     Toast.makeText(RegistrationActivity.this, "Password Does Not Match", Toast.LENGTH_SHORT).show();
                 }
                 else {
-                    registerUser();
+                    showOtp();
+                    //registerUser();
                 }
             }
         });
@@ -161,6 +188,120 @@ public class RegistrationActivity extends AppCompatActivity {
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinDistrict.setAdapter(adapter2);
         spinDistrict.setSelection(0);
+    }
+    private void showOtp() {
+        final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(activity);
+        bottomSheetDialog.setContentView(R.layout.otp_bottom_layout);
+
+        OtpTextView otpTextview = bottomSheetDialog.findViewById(R.id.otpTextview);
+        TextView tvNumber = bottomSheetDialog.findViewById(R.id.tvNumber);
+        Button btnContinue = bottomSheetDialog.findViewById(R.id.btnContinue);
+        tvNumber.setText("OTP sent to "+etMobile.getText().toString().trim());
+
+        btnContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (otpTextview.getOTP().length() == 6){
+                    if (!mVerificationId.equals("")){
+                        verifyPhoneNumberWithCode(mVerificationId,otpTextview.getOTP().toString());
+
+                    }
+                    else {
+                        Toast.makeText(activity, "Invalid OTP", Toast.LENGTH_SHORT).show();
+                    }
+
+
+                }
+                else {
+                    Toast.makeText(activity, "Enter OTP", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+        bottomSheetDialog.setCancelable(false);
+        bottomSheetDialog.show();
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential credential) {
+                // This callback will be invoked in two situations:
+                // 1 - Instant verification. In some cases the phone number can be instantly
+                //     verified without needing to send or enter a verification code.
+                // 2 - Auto-retrieval. On some devices Google Play services can automatically
+                //     detect the incoming verification SMS and perform verification without
+                //     user action.
+                Log.d(TAG, "onVerificationCompleted:" + credential);
+
+                signInWithPhoneAuthCredential(credential);
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+                // This callback is invoked in an invalid request for verification is made,
+                // for instance if the the phone number format is not valid.
+                Log.w(TAG, "onVerificationFailed", e);
+
+                if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                    // Invalid request
+                } else if (e instanceof FirebaseTooManyRequestsException) {
+                    // The SMS quota for the project has been exceeded
+                }
+
+                // Show a message and update the UI
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String verificationId,
+                                   @NonNull PhoneAuthProvider.ForceResendingToken token) {
+                // The SMS verification code has been sent to the provided phone number, we
+                // now need to ask the user to enter the code and then construct a credential
+                // by combining the code with a verification ID.
+                Log.d(TAG, "onCodeSent:" + verificationId);
+                mVerificationId = verificationId;
+
+            }
+        };
+        startPhoneNumberVerification("+91"+etMobile.getText().toString().trim());
+
+    }
+    private void verifyPhoneNumberWithCode(String verificationId, String code) {
+        // [START verify_with_code]
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+        signInWithPhoneAuthCredential(credential);
+        // [END verify_with_code]
+    }
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+
+                            registerUser();
+                            // Update UI
+                        } else {
+                            // Sign in failed, display a message and update the UI
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                // The verification code entered was invalid
+                            }
+                        }
+                    }
+                });
+    }
+    private void startPhoneNumberVerification(String phoneNumber) {
+        // [START start_phone_auth]
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(phoneNumber)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+        // [END start_phone_auth]
     }
 
     private void setSpinnerList()
@@ -243,12 +384,12 @@ public class RegistrationActivity extends AppCompatActivity {
                     JSONObject jsonObject = new JSONObject(response);
                     if (jsonObject.getBoolean(Constant.SUCCESS)) {
                         Toast.makeText(activity, jsonObject.getString(Constant.MESSAGE), Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(activity, OtpActivity.class));
-                        finish();
                     }
                     else {
                         Toast.makeText(activity, jsonObject.getString(Constant.MESSAGE), Toast.LENGTH_SHORT).show();
                     }
+                    startActivity(new Intent(activity, LoginActivity.class));
+                    finish();
                 } catch (JSONException e){
                     e.printStackTrace();
                 }
